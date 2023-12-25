b66e7f92877ea9607a2b09d85af601a0f49fd741
pantat88 <132797949+pantat88@users.noreply.github.com>
2023-12-26 03:14:28 +0700

    instant PNG info

--- scripts/fast-pnginfo.py
+++ scripts/fast-pnginfo.py
@@ -1,7 +1,7 @@
-import gradio as gr
-from modules import script_callbacks
 import modules.generation_parameters_copypaste as parameters_copypaste
+from modules import script_callbacks
 from modules import extensions
+import gradio as gr
 import os
 
 def get_self_extension():
@@ -14,44 +14,69 @@ def get_self_extension():
         if ext.path in filepath:
             return ext
 
-
 def on_ui_tabs():
-
-  ext = get_self_extension()
-  if ext is None:
-      return []
-  js_ = [f'{x.path}?{os.path.getmtime(x.path)}' for x in ext.list_files('js', '.js')]
-  js_.insert(0, ext.path)
-
-
-  with gr.Blocks(analytics_enabled=False) as fast_pnginfo:
-    with gr.Row():
-      with gr.Column(scale=2):
-        gr.HTML(value='\n'.join(js_), elem_id="fastpng_js_path", visible=False)
-        gr.Markdown(
-        f"""
-        <center>
-        
-        <h3>Fast PNG Info ⚡</h3>
-        <p>Steal 'em quick, by using <b>JavaScript</b> instead of Python.</br>Currently only supports PNG from webui. Param from NovelAI soon.</p>     
-        
-        </center>
-        """)
-    with gr.Row().style(equal_height=False):
-      with gr.Column(variant='panel'):
-        image = gr.Image(elem_id="fastpnginfo_image", label="Source", source="upload", interactive=True, type="pil")
-        # image.change(lambda x: x, _js='(e)=>{console.log(e);fastpngprocess()}')
-
-      with gr.Column(variant='panel'):
-        submit = gr.Button(label="Submit", elem_id="fastpnginfo_submit", interactive=True, variant="primary", visible=True)
-        fast_generation_info = gr.Textbox(label="Parameters", visible=True, elem_id="fastpnginfo_generation_info", interactive=False)
+    ext = get_self_extension()
+    if ext is None:
+        return []
+    
+    js_ = [f'{x.path}?{os.path.getmtime(x.path)}' for x in ext.list_files('js', '.js')]
+    js_.insert(0, ext.path)
+    
+    with gr.Blocks(analytics_enabled=False) as fast_pnginfo:
         with gr.Row():
-          buttons = parameters_copypaste.create_buttons(["txt2img", "img2img", "inpaint", "extras"])
+            with gr.Column(scale=1):
+                gr.HTML(elem_id="fastpng_js_path", value='\n'.join(js_), visible=False)
+                gr.Markdown(
+                f"""
+                <center>
+                <h3 style="font-size: 17px;">Fast PNG Info ⚡</h3>     
+                </center>
+                """)
 
-        for tabname, button in buttons.items():
-          parameters_copypaste.register_paste_params_button(parameters_copypaste.ParamBinding(
-              paste_button=button, tabname=tabname, source_text_component=fast_generation_info, source_image_component=image,
-          ))
-  return [(fast_pnginfo, "Fast PNG Info", "fast_pnginfo")];
+        with gr.Row(equal_height=False):
+            with gr.Column(variant='panel'):
+                image = gr.Image(
+                    elem_id="fastpnginfo_image",
+                    source="upload",
+                    interactive=True,
+                    type="pil")
+
+                with gr.Row(variant='compact'):
+                    buttons = parameters_copypaste.create_buttons(
+                        ["txt2img", "img2img", "inpaint", "extras"])
+
+            with gr.Column(scale=2):
+                submit = gr.Button(
+                    elem_id="fastpnginfo_submit",
+                    label="submit",
+                    interactive=True,
+                    variant="primary",
+                    visible=False)
+
+                fast_geninfo = gr.Textbox(
+                    elem_id="fastpnginfo_geninfo",
+                    visible=True,
+                    show_label=False,
+                    max_lines=999,
+                    interactive=False)
+
+                for tabname, button in buttons.items():
+                    parameters_copypaste.register_paste_params_button(
+                        parameters_copypaste.ParamBinding(
+                        paste_button=button,
+                            tabname=tabname,
+                            source_text_component=fast_geninfo,
+                            source_image_component=image))
+
+            js = """
+            (e) => {  
+                fastpngprocess(e);
+                document.querySelector("#fastpnginfo_submit").click();
+                document.querySelector("#fastpnginfo_geninfo").style.visibility = "visible";
+            }
+            """
+        image.change(fn=None, inputs=[image], _js=js, outputs=[fast_geninfo])
+        
+    return [(fast_pnginfo, "Fast PNG Info", "fast_pnginfo")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)

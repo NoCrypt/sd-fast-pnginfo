7236ffc3335bbdc8d0db28d45a2a4f3395c1a785
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-05-21 12:21:45 +0900

    Simplify, Restore Raw pnginfo in HTML (#10)
    * remove print
    
    * installer create dir on lib download
    
    * remove placeholder
    
    * add /js to .gitignore
    
    * remove unnecessary
    
    * move plaintext_to_html to js
    
    * simplify
    
    * remove unused logic
    
    * remove needless styling of info
    
    stylization of the info brakes the syntax
    
    * remove chance of name space collision
    
    * simplify

--- javascript/fast-pnginfo.js
+++ javascript/fast-pnginfo.js
@@ -1,38 +1,10 @@
-fastpnginfo_loaded = false;
-ExifReader = null;
-
-async function load_fastpnginfo(txt_output_el) {
-  if (ExifReader == null) {
-    let paths = gradioApp().querySelector("#fastpng_js_path");
-    const scripts = paths.textContent.trim().split("\n");
-    scripts.shift();
-    const df = document.createDocumentFragment();
-
-    for (let src of scripts) {
-      const script = document.createElement("script");
-      script.async = true;
-      script.type = "module";
-      script.src = `file=${src}`;
-      df.appendChild(script);
-    }
-
-    txt_output_el.appendChild(df);
-    await import(`/file=${scripts[0]}`);
-    fastpnginfo_loaded = true;
-  }
-}
-
-fastpngprocess = function () {};
-
-function round(v) { return Math.round(v * 10000) / 10000 }
-
 function convertNAI(input) {
   const re_attention = /\{|\[|\}|\]|[^\{\}\[\]]+/gmu;
   let text = input.replaceAll("(", "\(").replaceAll(")", "\)").replace(/\\{2,}(\(|\))/gim, '\$1');
   let res = [], curly_brackets = [], square_brackets = [];
   const curly_bracket_multiplier = 1.05, square_bracket_multiplier = 1 / 1.05;
   function multiply_range(start, multiplier) {
-    for (let pos = start; pos < res.length; pos++) res[pos][1] = round(res[pos][1] * multiplier);
+    for (let pos = start; pos < res.length; pos++) res[pos][1] = Math.round(res[pos][1] * multiplier * 10000) / 10000;
   }
   for (const match of text.matchAll(re_attention)) {
     let word = match[0];
@@ -52,7 +24,7 @@ function convertNAI(input) {
       res.splice(i + 1, 1);
     } else i++;
   }
-  
+
   let result = "";
   for (let i = 0; i < res.length; i++) {
     if (res[i][1] == 1.0) result += res[i][0];
@@ -61,97 +33,84 @@ function convertNAI(input) {
   return result;
 }
 
-onUiLoaded(async function () {
-  const app = gradioApp();
-  if (!app || app === document) return;
+async function fastpnginfo_parse_image() {
+  let output = "";
 
-  const img_input_el = app.querySelector("#fastpnginfo_image > div > div > input");
-  const txt_output_el = app.querySelector("#fastpnginfo_geninfo  > label > textarea");
-  const submit_el = app.querySelector("#fastpnginfo_submit");
+  let img_el = gradioApp().querySelector("#fastpnginfo_image > div[data-testid='image'] > div > img");
+  try {
+    let response = await fetch(img_el.src);
+    let img_blob = await response.blob();
+    let arrayBuffer = await img_blob.arrayBuffer();
 
-  if (!img_input_el || !txt_output_el) return;
+    let tags = ExifReader.load(arrayBuffer);
+    if (tags) {
+      const box = document.querySelector("#fastpnginfo_html");
 
-  img_input_el.addEventListener("change", fastpnginfo_process_image);
+      if (tags.parameters) {
+        output = tags.parameters.description;
 
-  await load_fastpnginfo(txt_output_el);
+      } else if (tags.UserComment && tags.UserComment.value) {
+        const ray = tags.UserComment.value;
+        const result = [];
+        var ar = ray;
+        var pos = ar.indexOf(0) + 1;
 
-  submit_el.addEventListener("click", async function () {
-    let img_el = app.querySelector("#fastpnginfo_image > div[data-testid='image'] > div > img");
+        for(var i = pos; i < ar.length; i += 2) {
+          var inDEX = ar[i];
+          var nEXT = ar[i + 1];
 
-    try {
-      let response = await fetch(img_el.src);
-      let img_blob = await response.blob();
-      let arrayBuffer = await img_blob.arrayBuffer();
-      let tags = ExifReader.load(arrayBuffer);
-
-      if (tags) {
-        let output = "";
-
-        if (tags.parameters) {
-          output = tags.parameters.description;
-          
-        } else if (tags.UserComment && tags.UserComment.value) {
-          const ray = tags.UserComment.value;
-          const result = [];
-          var ar = ray;
-          var pos = ar.indexOf(0) + 1;
-
-          for(var i = pos; i < ar.length; i += 2) {
-            var inDEX = ar[i];
-            var nEXT = ar[i + 1];
-
-            if(inDEX === 0 && nEXT === 32) {
-              result.push(32);
-              continue;
-            }
-
-            let vaLUE = inDEX * 256 + nEXT;
-            result.push(vaLUE);
+          if(inDEX === 0 && nEXT === 32) {
+            result.push(32);
+            continue;
           }
 
-          const userComment = new TextDecoder("utf-16").decode(new Uint16Array(result));
-          output = userComment.trim().replace(/^UNICODE[\x00-\x20]*/, "");
-
-        } else if (tags["Software"] && 
-                   tags["Software"].description === "NovelAI" && 
-                   tags.Comment && 
-                   tags.Comment.description) {
-
-          const nai = JSON.parse(tags.Comment.description);
-          nai.sampler = "Euler";
-
-          output = convertNAI(nai["prompt"])
-            + "\nNegative prompt: " + convertNAI(nai["uc"])
-            + "\nSteps: " + (nai["steps"])
-            + ", Sampler: " + (nai["sampler"])
-            + ", CFG scale: " + (parseFloat(nai["scale"]).toFixed(1))
-            + ", Seed: " + (nai["seed"])
-            + ", Size: " + (nai["width"]) + "x" + (nai["height"])
-            + ", Clip skip: 2, ENSD: 31337";
-
-        } else {
-          output = "Nothing To See Here";
+          let vaLUE = inDEX * 256 + nEXT;
+          result.push(vaLUE);
         }
 
-        txt_output_el.value = output;
-        txt_output_el.dispatchEvent(new Event("input", { bubbles: true }));
+        const userComment = new TextDecoder("utf-16").decode(new Uint16Array(result));
+        output = userComment.trim().replace(/^UNICODE[\x00-\x20]*/, "");
+
+      } else if (tags["Software"] &&
+                 tags["Software"].description === "NovelAI" &&
+                 tags.Comment &&
+                 tags.Comment.description) {
+
+        const nai = JSON.parse(tags.Comment.description);
+        nai.sampler = "Euler";
+
+        output = convertNAI(nai["prompt"])
+          + "\nNegative prompt: " + convertNAI(nai["uc"])
+          + "\nSteps: " + (nai["steps"])
+          + ", Sampler: " + (nai["sampler"])
+          + ", CFG scale: " + (parseFloat(nai["scale"]).toFixed(1))
+          + ", Seed: " + (nai["seed"])
+          + ", Size: " + (nai["width"]) + "x" + (nai["height"])
+          + ", Clip skip: 2, ENSD: 31337";
+
+      } else {
+        output = null;
+        box.style.opacity = "0";
       }
 
-      return tags;
-
-    } finally {
-      return app;
+      if (output) {
+        box.style.opacity = "1";
+        const txt_output_el = gradioApp().querySelector("#fastpnginfo_geninfo  > label > textarea");
+        txt_output_el.value = output;
+        updateInput(txt_output_el);
+        const fastpnginfo_geninfo_html = gradioApp().querySelector("#fastpnginfo_geninfo_html");
+        output = fastpnginfo_plaintext_to_html(output)
+        fastpnginfo_geninfo_html.classList.add('prose');
+        fastpnginfo_geninfo_html.innerHTML = output;
+      }
     }
-  });
+  }finally {
 
-  async function fastpnginfo_process_image() {
-    let img_el = app.querySelector("#fastpnginfo_image > div[data-testid='image'] > div > img");
-
-    while (!img_el) {
-      await new Promise(r => setTimeout(r, 100));
-      img_el = app.querySelector("#fastpnginfo_image > div[data-testid='image'] > div > img");
-    }
-
-    await new Promise(r => setTimeout(r, 100));
   }
-});
+}
+
+function fastpnginfo_plaintext_to_html(inputs) {
+  inputs = inputs.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  inputs = inputs.replace(/\n/g, '<br>');
+  return `<div style="padding: 5px;">${inputs}</p></div>`;
+}
--- scripts/fast-pnginfo.py
+++ scripts/fast-pnginfo.py
@@ -1,31 +1,13 @@
-import modules.generation_parameters_copypaste as parameters_copypaste
+import modules.generation_parameters_copypaste as parameters_copypaste  # noqa: generation_parameters_copypaste is the ailes to infotext_utils
 from modules.ui_components import FormRow, FormColumn
-from modules import script_callbacks
-from modules import extensions
+from modules import script_callbacks, scripts
+from pathlib import Path
 import gradio as gr
-import os
 
-def get_self_extension():
-    if '__file__' in globals():
-        filepath = __file__
-    else:
-        import inspect
-        filepath = inspect.getfile(lambda: None)
-    for ext in extensions.active():
-        if ext.path in filepath:
-            return ext
 
 def on_ui_tabs():
-    ext = get_self_extension()
-    if ext is None:
-        return []
-    
-    js_ = [f'{x.path}?{os.path.getmtime(x.path)}' for x in ext.list_files('js', '.js')]
-    js_.insert(0, ext.path)
-    
     with gr.Blocks(analytics_enabled=False) as fast_pnginfo:
-        gr.HTML(elem_id="fastpng_js_path", value='\n'.join(js_), visible=False)
-        gr.Button(elem_id="fastpnginfo_submit", label="submit", interactive=True, visible=False)
+
         geninfo = gr.Textbox(elem_id="fastpnginfo_geninfo", visible=False)
 
         with FormRow(equal_height=False):
@@ -34,50 +16,32 @@ def on_ui_tabs():
 
                 with FormRow(variant='compact'):
                     buttons = parameters_copypaste.create_buttons(["txt2img", "img2img", "inpaint", "extras"])
-        
+
             with gr.Column(variant='panel', scale=2, elem_id="fastpnginfo_html"):
-                display = gr.HTML()
+                gr.HTML(elem_id="fastpnginfo_geninfo_html")
 
             for tabname, button in buttons.items():
                 parameters_copypaste.register_paste_params_button(parameters_copypaste.ParamBinding(
                     paste_button=button, tabname=tabname, source_text_component=geninfo, source_image_component=image))
-            
-        image.change(fn=None, inputs=[image], outputs=[geninfo], _js="""
-        (e) => {
-            fastpngprocess(e);
-            document.querySelector("#fastpnginfo_submit").click();
-            document.querySelector("#fastpnginfo_geninfo").style.visibility = "visible";
-        }
-        """)
-        
-        geninfo.change(fn=None, inputs=[geninfo], outputs=[display], _js="""
-        function plaintext_to_html(inputs) {
-            var box = document.querySelector("#fastpnginfo_html");
 
-            var pr = '<b style="display: block; margin-bottom: 4px;">Prompt</b>\\n\\n';
-            var np = `<br><b style="display: block; margin-top: 15px; margin-bottom: 4px;">Negative Prompt</b>`;
-            var st = `<b style="display: block; margin-top: 15px; margin-bottom: 4px;">Settings</b>`;
-
-            if (inputs === undefined || inputs === null || inputs.trim() === '') {
-                box.style.opacity = "0";
-
-            } else {
-                if (inputs.includes("Nothing To See Here")) {
-                    pr = '';
-                }
-
-                box.style.opacity = "1";
-                inputs = inputs.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-                inputs = inputs.replace(/\\n/g, '<br>');
-                inputs = inputs.replace(/<br>Negative prompt:/, np);
-                inputs = inputs.replace(/Steps:/, match => st + match);                                    
-            }
-
-            return `<div style="padding: 5px;">${pr}<p>${inputs}</p></div>`;
-        }
-        """)
+        image.change(fn=None, _js='fastpnginfo_parse_image')
 
     return [(fast_pnginfo, "Fast PNG Info", "fast_pnginfo")]
 
+
 script_callbacks.on_ui_tabs(on_ui_tabs)
-print("\033[38;5;208m▶\033[0m Fast PNG Info")
+
+
+def download_exif_reader():
+    exif_reader = Path(scripts.basedir()) / 'javascript' / 'exif-reader.js'
+    if not exif_reader.exists():
+        from urllib.request import urlopen
+        import shutil
+        exif_reader.parent.mkdir(parents=True, exist_ok=True)
+        print(f"Downloading Fast PNG info requirement: \033[38;5;208mexif-reader.js\033[0m")
+        url = "https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js"
+        with urlopen(url) as response, open(exif_reader, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+
+
+download_exif_reader()

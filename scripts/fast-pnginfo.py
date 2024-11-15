a7d6cbbabf5355926bc2f48b3484e79cf920102d
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-15 20:01:35 +0700

    Update fast-pnginfo.py

--- scripts/fast-pnginfo.py
+++ scripts/fast-pnginfo.py
@@ -6,13 +6,13 @@ import gradio as gr
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as fast_pnginfo:
         with FormRow(equal_height=False):
-            with FormColumn(variant="compact"):
+            with FormColumn(variant="compact", scale=3):
                 image = gr.Image(elem_id="fastpnginfo_image", source="upload", interactive=True, type="pil", show_label=False)
 
                 with FormRow(variant="compact", elem_id="fastpngSendButton"):
                     buttons = tempe.create_buttons(["txt2img", "img2img", "inpaint", "extras"])
 
-            with gr.Column(variant="panel", scale=2, elem_id="fastpnginfo_panel"):
+            with gr.Column(variant="panel", scale=7, elem_id="fastpnginfo_panel"):
                 geninfo = gr.Textbox(elem_id="fastpnginfo_geninfo", label="RAW", visible=False)
                 gr.HTML(elem_id="fastpnginfo_html")
 

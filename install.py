28e046703a565990c0179f3925370f71513f1334
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-27 18:02:58 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,16 @@
 import urllib.request, shutil
 from pathlib import Path
 
-req = Path(__file__).parent / "javascript/exif-reader.js"
-url = "https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js"
+req1 = Path(__file__).parent / "javascript/exif-reader.js"
+req2 = Path(__file__).parent / "javascript/exif-reader-LICENSE"
 
-if not req.exists():
-    with urllib.request.urlopen(url) as r, open(req, 'wb') as o:
+url1 = "https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js"
+url2 = "https://raw.githubusercontent.com/mattiasw/ExifReader/main/LICENSE"
+
+if not req1.exists():
+    with urllib.request.urlopen(url1) as r, open(req1, 'wb') as o:
+        shutil.copyfileobj(r, o)
+
+if not req2.exists():
+    with urllib.request.urlopen(url2) as r, open(req2, 'wb') as o:
         shutil.copyfileobj(r, o)
